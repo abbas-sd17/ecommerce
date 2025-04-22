efc5f66b65ca88c152887fd4e2a7abb9db730c73
syed4.raza <syed4.raza@ril.com>
2025-04-22 23:10:18 +0530

    rest framework

--- products/urls.py
+++ products/urls.py
@@ -1,8 +1,10 @@
 
 from django.urls import  path
-
 from products import views
 
+
 urlpatterns = [
-    path('hello/',views.hello)
+    path('hello/',views.hello),
+    path('allproducts/',views.getproducts),
+    path('products/<int:product_id>/', views.get_product_by_id),
 ]
--- products/views.py
+++ products/views.py
@@ -1,8 +1,11 @@
 from django.http import HttpResponse
 
 from django.shortcuts import render
-
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from .serializers import ProductSerializer
 from products.models import Products
+from rest_framework import status
 
 
 # Create your views here.
@@ -16,3 +19,18 @@ def hello(request):
     print( d.name)
 
     return HttpResponse("hello world")
+
+@api_view(['GET'])
+def getproducts(request):
+    data=Products.objects.all()
+    serializer = ProductSerializer(data, many=True)
+    return Response(serializer.data)
+
+@api_view(['GET'])
+def get_product_by_id(request, product_id):
+    try:
+        product = Products.objects.get(id=product_id)
+        serializer = ProductSerializer(product)
+        return Response(serializer.data)
+    except Products.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
